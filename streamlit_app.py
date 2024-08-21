eb3bf1849d18146533a763652d0df3e70733ef8a
George <george.a.czelusniak@exxonmobil.com>
2024-08-21 10:32:25 -0300

    Atualizar o streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
+
 # Import python packages
 
 import streamlit as st
 
 from snowflake.snowpark.functions import col
+
+import requests
 
  
 
@@ -61,6 +64,14 @@ if ingredients_list: #if is not null: then do this
     for fruit_chosen in ingredients_list:
 
         ingredients_string += fruit_chosen + ' '
+
+        st.subheader(fruit_chosen + 'Nutrition Information')
+
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
+
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+ 
 
        
 
